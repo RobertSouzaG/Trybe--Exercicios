5dabe0edbf176051b0cbbd1efaf4c4d1a1931e53
Robert Souza G <souzarobert437@gmail.com>
2023-03-06 10:23:37 -0300

    Atividades

---
+++ Modulo-Fundamentos/Seção3-Intro.JavaScript/Dia5-JS.Es6/atividades.js
@@ -0,0 +1,8 @@
+function imprimeIdade() {
+    let idade = 30
+    for (let idade = 30; idade <= 40; idade += 1) {
+      console.log('Idade dentro do for:', idade)
+    }
+    // console.log('Idade fora do for:', idade) // retire essa linha ao implementar o tipo de variável correta.
+  }
+  imprimeIdade()
